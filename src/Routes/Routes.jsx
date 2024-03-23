5e53c543a5361ee1dbe4e9fd1c9397208b72bedc
Basit612 <sheikhabdulbasit612@gmail.com>
2024-03-23 17:17:04 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/donation",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/statistcs",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default router
